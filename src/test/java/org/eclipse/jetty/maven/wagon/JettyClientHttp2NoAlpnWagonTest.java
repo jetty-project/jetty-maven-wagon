75f0c3b4bba5b1ebd661644907864146b314641f
olivier lamy <olamy@apache.org>
2020-04-20 20:40:50 +1000

    not using alpn neither
    Signed-off-by: olivier lamy <olamy@apache.org>

---
+++ src/test/java/org/eclipse/jetty/maven/wagon/JettyClientHttp2NoAlpnWagonTest.java
@@ -0,0 +1,60 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2019 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+package org.eclipse.jetty.maven.wagon;
+
+import org.apache.maven.wagon.Wagon;
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+
+public class JettyClientHttp2NoAlpnWagonTest
+    extends JettyClientHttpsWagonTest
+{
+
+    @Override
+    protected String getWagonRoleHint()
+    {
+        return "h2";
+    }
+
+    @Override
+    protected Wagon getWagon()
+        throws Exception
+    {
+        JettyClientH2MavenWagon wagon = (JettyClientH2MavenWagon)super.getWagon();
+        wagon.setUseAlpn(false);
+        return wagon;
+    }
+
+    @Override
+    protected Connector newHttpsConnector(boolean needClientAuth)
+    {
+        HttpConfiguration httpsConfig = new HttpConfiguration();
+        httpsConfig.addCustomizer(new SecureRequestCustomizer());
+        HTTP2ServerConnectionFactory h2 = new HTTP2ServerConnectionFactory(httpsConfig);
+        ServerConnector http2Connector =
+            new ServerConnector(server,getSslContextFactory(needClientAuth), h2);
+
+        return http2Connector;
+    }
+}
